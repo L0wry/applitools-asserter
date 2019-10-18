a33107ffcf63c0945d5d84fb64c03533ea62b98e
L0wry <matthewlowry@hotmail.co.uk>
2019-10-18 16:53:22 +0100

    Feat: applitools asserter

--- src/applitools.js
+++ src/applitools.js
@@ -3,20 +3,30 @@ import urlJoin from 'url-join';
 
 const APPLITOOLS_BASE_URL = 'https://eyes.applitools.com';
 
+const isPassing = (completedCount, passedCount) => completedCount === passedCount;
+
 export default async (request, reply) => {
     const { apiKey, batchId } = request.query
 
     if (!batchId) {
-        reply.code(400).send({ error: 'missing batchID' }) 
-        return;
+        return reply.code(400).send({ error: 'missing batchID' })
     }
     if (!apiKey) {
-        reply.code(400).send({ error: 'missing api key ' })
-        return;
+        return reply.code(400).send({ error: 'missing api key ' })
     }
 
     const url = urlJoin(APPLITOOLS_BASE_URL, 'api', 'sessions', 'batches', batchId, 'bypointerid', `?apikey=${apiKey}`)
-    const res = axios.get(url)
-    
-    reply.code(200).send(res)
+    const res = await axios.get(url)
+
+    const { isCompleted: isTestCompleted, passedCount, unresolvedCount, completedCount, failedCount } = res.data;
+    if (isTestCompleted) {
+
+        if (isPassing(completedCount, passedCount)) {
+            return reply.code(200).send({ status: 'passed' });
+        }
+        
+        return reply.code(200).send({ status: 'failed', reason: `unresolved: ${unresolvedCount} || failed: ${failedCount}`});
+    } else {
+        return reply.code(200).send({ status: 'failed', reason: `test batch: ${batchId} is still running` })
+    }
 }
--- src/applitools.test.js
+++ src/applitools.test.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 import nock from "nock"
 import applitoolsAsserter from "./applitools"
+import conflictsMock from './_mocks_/conflicts';
+import passingMock from './_mocks_/passing';
 
 const PORT = 3010;
 const BASE_URL = 'http://localhost';
@@ -20,24 +22,30 @@ const startServer = () => new Promise((resolve, reject) => {
     })
 })
 
+const nockApplitools = (applitoolsResponse = {}) => {
+    const batchId = 'batchId';
+    const apiKey = 'apiKey';
+
+    const scope = nock('https://eyes.applitools.com')
+        .get(`/api/sessions/batches/${batchId}/bypointerid?apikey=${apiKey}`)
+        .reply(200, applitoolsResponse)
+
+    return axios.get(`${BASE_URL}:${PORT}?apiKey=${apiKey}&batchId=${batchId}`).then(res => {
+        expect(res.status).toBe(200)
+        scope.done()
+        return res;
+    }).catch(err => {
+        console.log(err)
+        throw err
+    })
+} 
+
 describe("Applitools asserter", () => {
     beforeAll(startServer)
     afterEach(() => nock.cleanAll())
     afterAll(() => fastify.close())
 
-    it('should send a request to applitools with batch and apikey', () => {
-        const batchId = 'batchId';
-        const apiKey = 'apiKey';
-
-        const scope = nock('https://eyes.applitools.com')
-            .get(`/api/sessions/batches/${batchId}/bypointerid?apikey=${apiKey}`)
-            .reply(200)
-
-        return axios.get(`${BASE_URL}:${PORT}?apiKey=${apiKey}&batchId=${batchId}`).then(res => {
-            expect(res.status).toBe(200)
-            scope.done()
-        })
-    })
+    it('should send a request to applitools with batch and apikey', () => nockApplitools());
 
     it('should throw if batch Id is missing', () => {
         const apiKey = 'apiKey';
@@ -47,6 +55,25 @@ describe("Applitools asserter", () => {
     it('should throw if api key is missing', () => {
         const batchId = 'batchId';
         return axios.get(`${BASE_URL}:${PORT}?batchId=${batchId}`).catch(err => expect(err.response.status).toBe(400))
+    })
+
+    it("should not assert when the tests are not complete", async () => {
+        const batchId = 'batchId';
+        const res = await nockApplitools({isCompleted: false})
+        expect(res.status).toBe(200)
+        expect(res.data).toEqual({status: 'failed', reason: `test batch: ${batchId} is still running`})
+    })
+
+    it("should assert when there are conflicts", async () => {
+        const res = await nockApplitools(conflictsMock)
+        expect(res.status).toBe(200)
+        expect(res.data).toEqual({ status: 'failed', reason: `unresolved: 5 || failed: 0` })
+    })
+
+    it("should pass when there are no conflicts", async () => {
+        const res = await nockApplitools(passingMock)
+        expect(res.status).toBe(200)
+        expect(res.data).toEqual({ status: 'passed'})
     })
 })
 
